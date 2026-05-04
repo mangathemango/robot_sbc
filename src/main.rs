a6b0bb4e40656eb133fed2bd85fbf015e6b27f5f
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-04 21:18:59 +0800

    feat: add test gyro detection
    This will definitely need refactoring soon

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use serialport::SerialPort;
 use std::io::Read;
 use std::time::Duration;
-
+use std::thread::sleep;
 fn main() {
+    
     let mut port = serialport::new("COM8", 115200)
         .timeout(Duration::from_millis(10))
         .open()
         .expect("Failed to open port");
 
+    // println!("Calibrating...");
+    // let cmd = [0xFF, 0xAA, 0x69, 0x88, 0xB5];
+    // port.write_all(&cmd).unwrap();
+    // let cmd = [0xFF, 0xAA, 0x01, 0x01, 0x00];
+    // port.write_all(&cmd).unwrap();
+    // sleep(Duration::from_secs(4));
+    // let cmd = [0xFF, 0xAA, 0x01, 0x00, 0x00];
+    // port.write_all(&cmd).unwrap();
+
+    // let cmd = [0xFF, 0xAA, 0x00, 0x00, 0x00];
+    // port.write_all(&cmd).unwrap();
+
     let mut buffer = [0u8; 1];
     let mut frame: Vec<u8> = Vec::new();
 
+    let mut start_yaw = None;
+    let mut yaw:f32 = 0.0;
+    let mut gy :f32 = 0.0;
+    let mut gz :f32 = 0.0;
     loop {
+        
         if let Ok(_) = port.read_exact(&mut buffer) {
             let byte = buffer[0];
 
@@ -19,11 +37,22 @@ fn main() {
                 if byte == 0x55 {
                     frame.push(byte);
                 }
+            } else if frame.len() == 1 {
+                if byte == 0x53 {
+                    frame.push(byte);
+                }
             } else {
                 frame.push(byte);
 
-                if frame.len() == 11 {
-                    parse_frame(&frame);
+                if frame.len() == 22 {
+                    (yaw,gy,gz) = parse_frame(&frame);
+                    if start_yaw == None {
+                        start_yaw = Some(yaw)
+                    }
+                    let mut yaw_offset = yaw - start_yaw.unwrap();
+                    if yaw_offset > 180.0  {yaw_offset -= 360.0}
+                    if yaw_offset < -180.0 {yaw_offset += 360.0}
+                    println!("Yaw: {:.2} \tAngular velocity -> y: {:.2}, z: {:.2}", yaw_offset, gy, gz);
                     frame.clear();
                 }
             }
@@ -31,25 +60,10 @@ fn main() {
     }
 }
 
-fn parse_frame(frame: &[u8]) {
-    let frame_type = frame[1];
+fn parse_frame(frame: &[u8]) -> (f32, f32, f32) {
+    let yaw = i16::from_le_bytes([frame[6],  frame[7] ]) as f32 / 32768.0 * 180.0;
+    let gy  = i16::from_le_bytes([frame[15], frame[16]]) as f32 / 32768.0 * 180.0;
+    let gz  = i16::from_le_bytes([frame[17], frame[18]]) as f32 / 32768.0 * 180.0;
 
-    match frame_type {
-        0x53 => {
-            let yaw   = i16::from_le_bytes([frame[6], frame[7]]) as f32 / 32768.0 * 180.0;
-
-            print!("Yaw: {:.2} \t", yaw);
-        }
-
-        0x52 => {
-            let gy = i16::from_le_bytes([frame[4], frame[5]]) as f32 / 32768.0 * 180.0;
-            let gz = i16::from_le_bytes([frame[6], frame[7]]) as f32 / 32768.0 * 180.0;
-
-            println!("Angular velocity -> y: {:.2}, z: {:.2}", gy, gz);
-        }
-
-        _ => {
-            // ignore others for now
-        }
-    }
+    (yaw, gy, gz)
 }
