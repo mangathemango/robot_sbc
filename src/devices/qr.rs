81462b02999b8500a084cebf841b3d62e6ac918b
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-06 15:20:05 +0800

    chore: forgot to switch back to a Vec

--- src/devices/qr.rs
+++ src/devices/qr.rs
@@ -5,7 +5,7 @@ pub fn try_read() -> Result<(), String> {
     let mut device = Device::open(path)
         .map_err(|e| format!("{}", e))?;
 
-    let mut buffer = String::new();
+    let mut buffer = Vec::new();
 
     loop {
         for ev in device.fetch_events().map_err(|e| format!("{}", e))? {
