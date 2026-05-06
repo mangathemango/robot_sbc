d91c61f5e82927fd8f7222900477365228fde434
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-06 14:49:43 +0800

    fix: error handling

--- src/devices/qr.rs
+++ src/devices/qr.rs
@@ -1,13 +1,14 @@
 use evdev::{Device, EventSummary};
 
-pub fn try_read() {
+pub fn try_read() -> Result<(), String> {
     let path = "/dev/input/by-id/usb-USBKey_Chip_USBKey_Module_202730041341-event-kbd";
-    let mut device = Device::open(path)?;
+    let mut device = Device::open(path)
+        .map_err(|e| format!("{}", e))?;
 
     let mut buffer = Vec::new();
 
     loop {
-        for ev in device.fetch_events()? {
+        for ev in device.fetch_events().map_err(|e| format!("{}", e))? {
             match ev.destructure() {
                 EventSummary::Key(_, keycode, value) => {
                     if value == 1 {
