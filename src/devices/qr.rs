0bb4001535d3cf31231d1c25f069cba569f8c1e5
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-06 15:44:45 +0800

    feat: add QrDriver module

--- src/devices/qr.rs
+++ src/devices/qr.rs
@@ -1,57 +1,88 @@
-use evdev::{Device, EventSummary, KeyCode, EvdevEnum};
 
-pub fn try_read() -> Result<(), String> {
-    let path = "/dev/input/by-id/usb-USBKey_Chip_USBKey_Module_202730041341-event-kbd";
-    let mut device = Device::open(path)
-        .map_err(|e| format!("{}", e))?;
+use evdev::{Device, EventSummary, KeyCode};
+const QR_READER_DOTENV_KEY = "QR_READER_PATH";
 
-    let mut buffer = String::new();
+#[derive(Debug, Clone, Default)]
+pub struct QrDriver {
+    device: Device
+}
 
-    loop {
-        for ev in device.fetch_events().map_err(|e| format!("{}", e))? {
-            match ev.destructure() {
-                EventSummary::Key(_, keycode, value) => {
-                    if value == 1 {
-                        match keycode {
-                            evdev::KeyCode::KEY_ENTER => {
-                                println!("QR: {:?}", buffer);
-                                buffer.clear();
-                            }
-                            _ => {
-                                if let Some(char) = keycode_to_ascii(keycode, false) {
-                                    buffer.push(char);
+impl QrDriver {
+    pub fn new() -> Result<Self, String> {
+        let path = dotenv::var(QR_READER_DOTENV_KEY)
+            .map_err(|e| format!("Env error: {}", e))?;
+        let device = Device::open(path)
+            .map_err(|e| format!("Open Qrcode device error: {}", e))?;
+
+        Ok(QrDriver { device })
+    }
+    pub fn reconnect(&mut self) -> Result<(), String> {
+        let path = dotenv::var(QR_READER_DOTENV_KEY)
+            .map_err(|e| format!("Env error: {}", e))?;
+        let device = Device::open(path)
+            .map_err(|e| format!("Open Qrcode device error: {}", e))?;
+
+        self.device = device;
+        Ok(())
+    }
+
+    pub fn try_read(&mut self) -> Result<String, String> {
+        let mut buffer = String::new();
+
+        loop {
+            for ev in self.device.fetch_events().map_err(|e| format!("Fetch event from Qr code error: {}", e))? {
+                match ev.destructure() {
+                    EventSummary::Key(_, keycode, value) => {
+                        if value == 1 {
+                            match keycode {
+                                evdev::KeyCode::KEY_ENTER => {
+                                    return Ok(buffer);
+                                }
+                                _ => {
+                                    if let Some(c) = Self::keycode_to_ascii(keycode) {
+                                        buffer.push(char);
+                                    }
                                 }
                             }
                         }
                     }
+                    _ => {}
                 }
-                _ => {}
             }
         }
     }
+
+
+
+    pub fn keycode_to_ascii(key: KeyCode) -> Option<char> {
+
+        let c = match key {
+            KeyCode::KEY_1 => '1',
+            KeyCode::KEY_2 => '2',
+            KeyCode::KEY_3 => '3',
+            KeyCode::KEY_4 => '4',
+            KeyCode::KEY_5 => '5',
+            KeyCode::KEY_6 => '6',
+            KeyCode::KEY_7 => '7',
+            KeyCode::KEY_8 => '8',
+            KeyCode::KEY_9 => '9',
+            KeyCode::KEY_0 => '0',
+
+            // The Qr reader inputs a "+" by doing KeyCode::Shift THEN a Keycode::Equal. 
+            // It's alr I'm just gonna hard code it
+            KeyCode::KEY_EQUAL => '+',
+
+            _ => return None,
+        };
+
+        // optional shift handling
+        Some(c)
+    }
 }
 
-pub fn keycode_to_ascii(key: KeyCode, shift: bool) -> Option<char> {
-
-    let c = match key {
-        KeyCode::KEY_1 => '1',
-        KeyCode::KEY_2 => '2',
-        KeyCode::KEY_3 => '3',
-        KeyCode::KEY_4 => '4',
-        KeyCode::KEY_5 => '5',
-        KeyCode::KEY_6 => '6',
-        KeyCode::KEY_7 => '7',
-        KeyCode::KEY_8 => '8',
-        KeyCode::KEY_9 => '9',
-        KeyCode::KEY_0 => '0',
-
-        // The Qr reader inputs a "+" by doing KeyCode::Shift THEN a Keycode::Equal. 
-        // It's alr I'm just gonna hard code it
-        KeyCode::KEY_EQUAL => '+',
-
-        _ => return None,
-    };
-
-    // optional shift handling
-    Some(if shift { c.to_ascii_uppercase() } else { c })
+#[derive(Debug, Clone, Default)]
+pub struct QrState {
+    pub driver_is_active: bool,
+    pub code: String,
+    pub error_msg: String
 }
