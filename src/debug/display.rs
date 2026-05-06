120a40be448be1d3e208465e4cef176f7eea8623
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-06 13:40:03 +0800

    chore: remove raw display

--- src/debug/display.rs
+++ src/debug/display.rs
@@ -141,8 +141,7 @@ pub fn draw_system(f: &mut Frame, area: Rect) {
     let mem_usage = (used_mem / total_mem) * 100.0;
 
     let text = format!(
-        "SYSTEM\n\nRAW: {:#?}\n\nCPU: {:.1}%\nRAM: {:.1}%\nTEMP: {}°C\n\nPROCS: {}",
-        sys,
+        "SYSTEM\n\nCPU: {:.1}%\nRAM: {:.1}%\nTEMP: {}°C (ideal: 50-70°C)\n\nPROCS: {}",
         sys.global_cpu_usage(),
         mem_usage,
         read_temperature().unwrap_or(0.0),
