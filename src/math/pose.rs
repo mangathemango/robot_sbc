83d6d0a97e760a5a16308cffdfb981b50542d41f
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-07 17:13:36 +0800

    feat: add Pose struct

---
+++ src/math/pose.rs
@@ -0,0 +1,27 @@
+use glam::Vec2;
+
+/// A struct representing where an object is and where it's facing
+#[derive(Debug, Clone, Copy, Default)]
+struct Pose {
+    position: Vec2,
+    rotation: f32 // in radians
+}
+
+impl Pose {
+    pub fn new(position: Vec2, rotation: f32) -> Self {
+        Pose {position, rotation}
+    }
+
+    pub fn forward(&self) -> Vec2 {
+        Vec2::from_angle(self.rotation)
+    }
+    pub fn right(&self) -> Vec2 {
+        self.forward().perp()
+    }
+    pub fn back(&self) -> Vec2 {
+        -self.forward()
+    }
+    pub fn left(&self) -> Vec2 {
+        -self.right()
+    }
+}   
