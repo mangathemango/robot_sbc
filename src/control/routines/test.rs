dbce6086e5ebf3fc1cd5024579d24ad066d89aaf
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-18 23:04:47 +0800

    chore: reset qr code after each test

--- src/control/routines/test.rs
+++ src/control/routines/test.rs
@@ -1,10 +1,13 @@
 use std::time::Duration;
 
-use crate::control::{actions::general::{RuntimeSequence, Sequence, WaitFor}, routines::{navigation::{move_back_to_start, move_to_qr}, utils::{beep, set_oled_display_text_qr, set_oled_display_text_start, set_oled_display_text_stop, wait_for_qr}}};
+use crate::{ROBOT, control::{actions::general::{OneShot, RuntimeSequence, Sequence, WaitFor}, routines::{navigation::{move_back_to_start, move_to_qr}, utils::{beep, set_oled_display_text_qr, set_oled_display_text_start, set_oled_display_text_stop, wait_for_qr}}}};
 
 pub fn test_sequence() -> Sequence {
     Sequence::new("Test Sequence")
         .then(set_oled_display_text_start())
+        .then(OneShot::new(|| {
+            ROBOT.lock_qr_state().code = None;
+        }))
         .then(wait_for_qr())
         .then(set_oled_display_text_qr())
         // .then(set_oled_display_text_stopq())
