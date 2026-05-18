498e2a89e8afe6f55c1676fa5afe33ef0a148ed6
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-19 00:14:07 +0800

    chore: try disabling stm32 thread

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ static ROBOT: Lazy<Robot> = Lazy::new(|| Robot::new());
 fn main() {
     // DEVICE THREADS
     // Thread to retrieve and send serial data to the STM32. Updates ROBOT.stm32_state and provides ROBOT.get_stm32_controller()
-    spawn_stm32_thread();
+    // spawn_stm32_thread();
 
     // Thread to retrieve raw data from the HWTCT101 gyroscope. Updates ROBOT.gyro_data
     spawn_gyro_thread();
