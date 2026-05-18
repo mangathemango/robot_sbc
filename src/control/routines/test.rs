c6a910303ee9dee5c5f6bf7351fe9ca2f38d8615
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-18 22:53:13 +0800

    feat: add qr display test sequence

--- src/control/routines/test.rs
+++ src/control/routines/test.rs
@@ -1,13 +1,10 @@
 use std::time::Duration;
 
-use crate::control::{actions::general::{RuntimeSequence, Sequence, WaitFor}, routines::{navigation::{move_back_to_start, move_to_qr}, utils::beep}};
+use crate::control::{actions::general::{RuntimeSequence, Sequence, WaitFor}, routines::{navigation::{move_back_to_start, move_to_qr}, utils::{beep, set_oled_display_text_qr, set_oled_display_text_start, set_oled_display_text_stop, wait_for_qr}}};
 
 pub fn test_sequence() -> Sequence {
     Sequence::new("Test Sequence")
-        .then(beep())
-        .then(WaitFor::new(Duration::from_millis(1000)))
-        .then(beep())
-        .then(WaitFor::new(Duration::from_millis(1000)))
-        .then(beep())
-        .then(WaitFor::new(Duration::from_millis(1000)))
+        .then(wait_for_qr())
+        .then(set_oled_display_text_qr())
+        // .then(set_oled_display_text_stopq())
 }
