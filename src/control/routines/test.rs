8280dc8dddf50fcde74c541273857de7503db541
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-05-18 23:46:08 +0800

    chore: add servo test script

--- src/control/routines/test.rs
+++ src/control/routines/test.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use crate::{ROBOT, control::{actions::general::{OneShot, RuntimeSequence, Sequence, WaitFor}, routines::{navigation::{move_back_to_start, move_to_qr}, utils::{beep, set_oled_display_text_qr, set_oled_display_text_start, set_oled_display_text_stop, wait_for_qr}}}};
+use crate::{ROBOT, control::{actions::{general::{OneShot, RuntimeSequence, Sequence, WaitFor}, rotate_claw::RotateClaw}, routines::{navigation::{move_back_to_start, move_to_qr}, utils::{beep, set_oled_display_text_qr, set_oled_display_text_start, set_oled_display_text_stop, wait_for_qr}}}};
 
 pub fn test_sequence() -> Sequence {
     Sequence::new("Test Sequence")
@@ -9,7 +9,9 @@ pub fn test_sequence() -> Sequence {
             let mut qr_state = ROBOT.lock_qr_state();
             qr_state.reset();
         }))
-        .then(wait_for_qr())
-        .then(set_oled_display_text_qr())
+        .then(RotateClaw::close())
+        .then(WaitFor::new(Duration::from_millis(1000)))
+        .then(RotateClaw::open())
+        .then(set_oled_display_text_stop())
         // .then(set_oled_display_text_stopq())
 }
